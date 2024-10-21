1eb123354dd5a00c767ae3906252a55b936edd82
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-10-22 00:22:17 +0100

    Test static files heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -173,12 +173,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 if 'USE_AWS' in os.environ:
@@ -194,6 +191,14 @@ if 'USE_AWS' in os.environ:
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+else:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import boto3
 from django.http import HttpResponse
 import os
+from decouple import config
 
 # Create your views here.
 
@@ -15,11 +16,11 @@ def index(request):
 def test_upload(request):
     s3 = boto3.client(
         's3',
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        aws_access_key_id=config('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY'),
         region_name='eu-north-1'
     )
     # Upload a sample file to test
     with open('static/test_file.txt', 'rb') as data:
-        s3.upload_fileobj(data, 'boutiqueado159', 'static/test_file.txt')
+        s3.upload_fileobj(data, 'boutiqueadowt', 'static/test_file.txt')
     return HttpResponse('File uploaded successfully.')
