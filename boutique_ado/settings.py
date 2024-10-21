1daba95be8f34d9159b47f70fb2bbc07b7587f22
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-10-21 20:35:09 +0100

    Test static files heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -189,9 +189,13 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
     # Override static and media URLS in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+   
 else:
     print("I am local")
     STATIC_URL = '/static/'
