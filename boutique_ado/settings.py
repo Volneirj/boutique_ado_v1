0f0662a6b4cb36a1be62293a3a582b83c3807e19
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-10-21 20:52:30 +0100

    Test static files heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -175,24 +175,23 @@ USE_TZ = True
 
 
 
+
+
 if 'USE_AWS' in os.environ:
-    print("I am in production")
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutiqueado159'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # static and media files
+    # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    # temp static root
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Override static and media URLS in production
+    # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
